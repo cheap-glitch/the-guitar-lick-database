8751503f9e5123c205bd044af55aa7dad5cbb9fd
cheap-glitch <cheap.glitch@gmail.com>
2020-04-19 19:28:26 +0200

    Fix colorscheme syntax

--- src/modules/colorscheme.js
+++ src/modules/colorscheme.js
@@ -6,11 +6,11 @@
 import colors from '@/styles/colorscheme-export.scss'
 
 export default {
-	'--color--bg':                   [colors.athensGray,  colors.ayu.ayuBg],
+	'--color--bg':                   [colors.athensGray,  colors.ayuBg],
 	'--color--bg--light':            ['#e9e9e9',          colors.ebonyClay],
 	'--color--bg--lighter':          ['#eee',             colors.oxfordBlue],
 
-	'--color--text':                 ['#222',             colors.regentStBlue],
+	'--color--text':                 ['#222',             colors.ayuFgText],
 	'--color--text--dark':           ['gray',             colors.nepal],
 
 	'--color--border':               ['lightgray',        colors.oxfordBlue],
