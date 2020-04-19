f356beb06249277cc3b7bf8353987fd20c09aade
cheap-glitch <cheap.glitch@gmail.com>
2020-04-19 19:00:40 +0200

    Separate SCSS variables and webpack export syntax in two different files

--- src/modules/colorscheme.js
+++ src/modules/colorscheme.js
@@ -3,10 +3,10 @@
  * modules/colorscheme.js
  */
 
-import colors from '@/styles/colorscheme.scss'
+import colors from '@/styles/colorscheme-export.scss'
 
 export default {
-	'--color--bg':                   [colors.athensGray,  colors.mirage],
+	'--color--bg':                   [colors.athensGray,  colors.ayu.ayuBg],
 	'--color--bg--light':            ['#e9e9e9',          colors.ebonyClay],
 	'--color--bg--lighter':          ['#eee',             colors.oxfordBlue],
 
